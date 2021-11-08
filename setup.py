9ee8bc4d8dc94f9323e699fd70d24b7376756443
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-11-08 12:17:17 +0600

    future package

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
   name="anonympy",
   version="0.1.0",
   packages=find_packages(include=["anonympy", "anonympy.*"]),
+  install_requires=['pandas>=1/0', 'scipy=1.1', 'matplotlib>=2.2.1,<3'],
+  python_requires='>=2.7, !=3.0.*, !=3.1.*'
 )
 
 # pip install -e .
+# pip freeze > requirements.txt
