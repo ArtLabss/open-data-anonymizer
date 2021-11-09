26fd1c5052ba1199f04c2104a8027784bc3a7b8f
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-11-09 13:45:39 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,17 @@ setup(
   version="0.1.0",
   packages=find_packages(include=["anonympy", "anonympy.*"]),
   install_requires=['pandas>=1.0', 'scipy=1.1', 'matplotlib>=2.2.1,<3'],
-  python_requires='>=3.6.*'
+  python_requires='>=3.6.*',
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    ],
 )
 
 # pip install -e .
