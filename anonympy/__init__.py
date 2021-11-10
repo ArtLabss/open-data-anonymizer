e82d5372fd56ce61a78636755733df91f5f84f48
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-11-10 13:57:03 +0600

    Update __init__.py

--- anonympy/__init__.py
+++ anonympy/__init__.py
@@ -4,3 +4,20 @@ such as tabular, text, images and sound.
 '''
 
 __version__ =  "0.1.0"
+
+# Check if all dependencies have been installed
+
+hard_dependencies = ("pandas", "faker", "cape-python")
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(f"{dependency}: {e}")
+
+if missing_dependencies:
+    raise ImportError(
+        "Unable to import required dependencies:\n" + "\n".join(missing_dependencies)
+    )
+del hard_dependencies, dependency, missing_dependencies
