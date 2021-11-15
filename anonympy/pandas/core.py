28e11efa865de45552d0c7e89097b9942c4b53da
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-11-15 21:36:48 +0600

    Base Class

--- anonympy/pandas/core.py
+++ anonympy/pandas/core.py
@@ -1,7 +1,7 @@
 import cape_privacy as cp
 from faker import Faker
 import pandas as pd
-from typing import List, Dict
+from typing import List, Dict, Union
 
 
 df = pd.DataFrame({
@@ -15,16 +15,14 @@ df = pd.DataFrame({
 
 class dfAnonymizer(object):
     """
-    Initializes a pd.DataFrame as a dfAnonymize object.
-    Set ``seed`` to an integer for reproducible results 
+    Initializes pandas DataFrame as a dfAnonymizer object.
 
     Parameters:
     ----------
-        df: pd.DataFrame
+        df: pandas DataFrame
         numeric_columns : list, default None
         categorical_columns : list, default None
         datetime_columns : list, default None
-        inplace : bool, default False
          
     Returns:
     ----------
@@ -42,50 +40,50 @@ class dfAnonymizer(object):
                  numeric_columns: List[str] = None,
                  categorical_columns: List[str] = None,
                  datetime_columns: List[str] = None,
-                 inplace: bool = False,
                  ):    
         if df.__class__.__name__ != "DataFrame":
             raise Exception(f"{df} is not a pandas DataFrame.")
 
         self._df = df
-        self._inplace = inplace
         if numeric_columns == None:
-            self._numeric_columns = df.select_dtypes(exclude=['object', 'datetime', 'category']).columns.tolist()
+            self._numeric_columns = self._df.select_dtypes(exclude=['object', 'datetime', 'category']).columns.tolist()
         if categorical_columns == None:
-            self._categorical_columns = df.select_dtypes(include=['object', 'category']).columns.tolist()
+            self._categorical_columns = self._df.select_dtypes(include=['object', 'category']).columns.tolist()
         if datetime_columns == None:
-            self._datetime_columns = df.select_dtypes(include=['datetime']).columns.tolist()
+            self._datetime_columns = self._df.select_dtypes(include=['datetime']).columns.tolist()
+        self.anonymized_columns = []
+        self.unanonymized_columns = self._df.columns.to_list()
 
 
     def get_numeric_columns(self) -> List:
         '''
-Return a subset of the DataFrame's columns which are numeric
+        Return a subset of the DataFrame's columns which are numeric
 
-Returns
-----------
-    List of columns with numeric values 
-'''
+        Returns
+        ----------
+            List of columns with numeric values 
+        '''
         return self._df[self._numeric_columns]
 
 
     def get_categorical_columns(self) -> List:
         '''
-Return a subset of the DataFrame's columns which are categorical
+        Return a subset of the DataFrame's columns which are categorical
 
-Returns
-----------
-List of columns with categorical values 
-'''
+        Returns
+        ----------
+            List of columns with categorical values 
+        '''
         return self._categorical_columns
 
 
     def get_datetime_columns(self) -> List:
         '''
-Return a subset of the DataFrame's columns which are datetime 
+        Return a subset of the DataFrame's columns which are datetime 
 
-Returns
-----------
-List of columns with datetime values 
+        Returns
+        ----------
+            List of columns with datetime values 
 '''
         return self._datetime_columns
 
@@ -97,13 +95,35 @@ List of columns with datetime values
             pass
         
 
-    def fake_data_manual(self, ser:pd.Series, method: str, locale: List[str] = ['en_US']) -> pd.Series:
+    def fake_data_manual(self, ser:pd.Series, method: str, locale: Union[str, List[str]] = ['en_US']) -> pd.Series:
+        '''
+        Anonymize pandas Series object using synthetic data generator 
+
+        Parameters
+        ----------
+            ser : pandas Series
+            method : str
+            locale : List[str], default ['en_US']
+
+        Returns
+        ----------
+            faked : pandas Series
+
+        '''
+        
         fake = Faker(locale=locale)
         method = getattr(fake, method)
         faked = ser.apply(lambda x: method())
         return faked
+    
+
+    def to_DataFrame(self):
+        return self._df
 
 
+    def info(self):
+        
+        
 
 
 
