455bcf9f46ac120f80307acfcd49090b94f2c1e0
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-12-16 11:15:44 +0600

    Delete build/lib/anonympy directory

--- build/lib/anonympy/tabular/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Data Anonymization for Pandas DataFrame
-'''
-
-from anonympy.tabular import core
-from anonympy.tabular import utils 
-from anonympy.tabular.core import dfAnonymizer
-
-__all__ = ["core", "utils", "dfAnonymizer"]
