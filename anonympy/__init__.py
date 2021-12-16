0b583c096498f059752e2326eaa45d2cb2774eb2
artkulak <artkulak444@gmail.com>
2021-12-16 11:38:05 +0600

    pypi files

--- anonympy/__init__.py
+++ anonympy/__init__.py
@@ -3,7 +3,7 @@ Package for data anonymization of different data types
 such as tabular, text, images and sound. 
 '''
 
-__version__ =  "0.1.2"
+__version__ =  "0.1.4"
 
 # Check if all dependencies have been installed
 
@@ -28,5 +28,6 @@ from anonympy import images
 from anonympy import sound 
 from anonympy import text
 from anonympy.tabular.core import dfAnonymizer
+from anonympy.tabular.utils import utils 
 
-__all__ = ["tabular", "tabular", "sound", "text", "dfAnonymizer"]
+__all__ = ["tabular", "tabular", "sound", "text", "dfAnonymizer", "utils"]
--- anonympy/tabular/__init__.py
+++ anonympy/tabular/__init__.py
@@ -3,7 +3,7 @@ Data Anonymization for Pandas DataFrame
 '''
 
 from anonympy.tabular import core
-from anonympy.tabular import utils 
+from anonympy.tabular.utils import utils 
 from anonympy.tabular.core import dfAnonymizer
 
 __all__ = ["core", "utils", "dfAnonymizer"]
