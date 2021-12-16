32405ea64810bb1e1cac72be7ca7633460471c88
Your Name <shakhansho.sabzaliev_2023@ucentralasia.org>
2021-12-16 12:31:04 +0600

    Uploading to pypi

--- anonympy/pandas/__init__.py
+++ anonympy/pandas/__init__.py
@@ -2,9 +2,7 @@
 Data Anonymization for Pandas DataFrame
 '''
 
-from anonympy.pandas import core
-from anonympy.pandas.core import *
 from anonympy.pandas.utils import utils 
 from anonympy.pandas.core import dfAnonymizer
 
-__all__ = ["core", "utils", "dfAnonymizer"]
+__all__ = ["utils", "dfAnonymizer"]
