eed1be4e03a3b9e3e7977aae94d6f2d7164c908e
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-12-17 10:17:11 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Natural Language :: English',
-    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
