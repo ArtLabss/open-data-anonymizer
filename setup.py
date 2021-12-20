857203a1adc51dbc1bbc8167c62af2eb08d70d6b
Shukur <shakhansho.sabzaliev_2023@ucentralasia.org>
2021-12-20 21:01:52 +0600

    Setup Script

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open('README.md', encoding='utf8') as readme_file:
     readme = readme_file.read()
 
 setup(
