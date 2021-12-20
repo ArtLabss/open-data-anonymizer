0ad0de504f4701d5e5e61ce2e7320a21ff7a2246
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-12-20 20:45:28 +0600

    Update __init__.py

--- anonympy/__init__.py
+++ anonympy/__init__.py
@@ -3,7 +3,7 @@ Package for data anonymization of different data types
 such as tabular, text, images and sound. 
 '''
 
-__version__ =  "0.1.7"
+__version__ =  "0.1.0"
 
 # Check if all dependencies have been installed
 
