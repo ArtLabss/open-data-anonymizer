944a97cca52d56275d040cbea3c5fe04c0d2319c
Shukur <shakhansho.sabzaliev_2023@ucentralasia.org>
2021-12-21 10:22:33 +0600

    pacakge specification

---
+++ anonympy/__init__.py
@@ -0,0 +1,31 @@
+'''
+Package for data anonymization of different data types 
+such as tabular, text, images and sound. 
+'''
+
+__version__ =  "0.1.1"
+
+# Check if all dependencies have been installed
+
+hard_dependencies = ("faker", "cape_privacy", "pandas", "numpy")
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(f"{dependency}: {e}")
+
+if missing_dependencies:
+    raise ImportError(
+        "Unable to import required dependencies:\n" + "\n".join(missing_dependencies)
+    )
+del hard_dependencies, dependency, missing_dependencies
+
+
+from anonympy import pandas
+from anonympy import images
+from anonympy import sound 
+from anonympy import text
+
+__all__ = ["pandas", "tabular", "sound", "text"]
