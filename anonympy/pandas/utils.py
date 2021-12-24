4ebb6fca079f30e34ce7702bf0e14af9f1992b23
Shukur Sabzaliev <78250180+shukkkur@users.noreply.github.com>
2021-12-24 17:30:41 +0600

    Update utils.py

--- anonympy/pandas/utils.py
+++ anonympy/pandas/utils.py
@@ -176,5 +176,5 @@ def load_dataset(size='small'):
 
         return df
     elif size == 'big':
-        df = pd.read_csv('https://raw.githubusercontent.com/ArtLabss/open-data-anonimizer/main/examples/files/new.csv')
+      df = pd.read_csv('https://raw.githubusercontent.com/ArtLabss/open-data-anonimizer/main/examples/files/new.csv', parse_dates=['birthdate']) 
         return df
