ea575ceb9876cc8892e46ada6f59df197848814c
Shukur <shakhansho.sabzaliev_2023@ucentralasia.org>
2022-01-12 12:04:59 +0600

    Image Anonymization Methods added

--- anonympy/images/core.py
+++ anonympy/images/core.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import os
+import random
 
 
 class imAnonymizer(object):
@@ -19,7 +21,7 @@ class imAnonymizer(object):
      ---------- 
      """
      def __init__(self, frame):
-          self.frame = frame
+          self.frame = frame.copy()
 
 
      def resize(self, new_width=500):
@@ -35,7 +37,7 @@ class imAnonymizer(object):
           '''
           face_cascade = cv2.CascadeClassifier(r"utils/cascade.xml")
           detections = face_cascade.detectMultiScale(self.frame,scaleFactor = scaleFactor, minNeighbors = minNeighbors)
-
+          print(detections)
           for face in detections:
                x,y,w,h = face
 
@@ -50,3 +52,59 @@ class imAnonymizer(object):
           '''
 
           cv2.imshow(fname, self.frame)
+          
+def add_noise(frame):
+    img = frame.copy()
+    # Getting the dimensions of the image
+    row , col, _ = img.shape
+     
+    # Randomly pick some pixels in the
+    # image for coloring them white
+    # Pick a random number between 300 and 10000
+    number_of_pixels = random.randint(300, 10000)
+    for i in range(number_of_pixels):
+       
+        # Pick a random y coordinate
+        y_coord=random.randint(0, row - 1)
+         
+        # Pick a random x coordinate
+        x_coord=random.randint(0, col - 1)
+         
+        # Color that pixel to white
+        img[y_coord][x_coord] = 255
+         
+    # Randomly pick some pixels in
+    # the image for coloring them black
+    # Pick a random number between 300 and 10000
+    number_of_pixels = random.randint(300 , 10000)
+    for i in range(number_of_pixels):
+       
+        # Pick a random y coordinate
+        y_coord=random.randint(0, row - 1)
+         
+        # Pick a random x coordinate
+        x_coord=random.randint(0, col - 1)
+         
+        # Color that pixel to black
+        img[y_coord][x_coord] = 0
+         
+    return img
+
+##img[y:y+h,x:x+w] = cv2.cvtColor(add_noise(cv2.cvtColor(img[y:y+h,x:x+w], cv2.COLOR_BGR2GRAY)), cv2.COLOR_GRAY2BGR )
+
+##def radius(x,y, w, h) -> tuple:
+##	pt1 = (x, y)
+##	pt2 = (x+w, y+h)
+##
+##	side_middle = x + w, (y + y + h) / 2
+##	center = find_middle(pt1, pt2)
+##	dis = side_middle[0] - center[0]
+##	
+##	return dis
+     
+
+##def find_middle(pt1: tuple, pt2: tuple) -> tuple:
+##	x1, y1 = pt1
+##	x2, y2 = pt2
+##	m1, m2 = int((x1 + x2)/2), int((y1 + y2)/2)
+##	return m1, m2
