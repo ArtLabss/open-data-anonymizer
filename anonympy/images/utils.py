c5e3b96c07bc27f8a7db7ad5c28fafaeb865461c
artkulak <artkulak444@gmail.com>
2022-01-19 18:22:10 +0600

    Supplementary Functions and Variables

--- anonympy/images/utils.py
+++ anonympy/images/utils.py
@@ -1,11 +1,14 @@
 # Supplementary functions and variables
 import random
+import numpy as np
+import cv2
 
 def find_middle(x, y, w, h) -> tuple:
 	x1, y1 = x, y
 	x2, y2 = x + w, y + h
 	m1, m2 = int((x1 + x2)/2), int((y1 + y2)/2)
 	return m1, m2
+
 
 def find_radius(x, y, w, h) -> tuple:
 	pt1 = (x, y)
@@ -16,6 +19,7 @@ def find_radius(x, y, w, h) -> tuple:
 	dis = side_middle[0] - center[0]
 	
 	return dis
+
 
 def sap_noise(frame):
     img = frame.copy()
@@ -55,3 +59,24 @@ def sap_noise(frame):
     return img
      
 
+def pixelated(image,  blocks = 20):
+          (h, w) = image.shape[:2]
+          xSteps = np.linspace(0, w, blocks + 1, dtype="int")
+          ySteps = np.linspace(0, h, blocks + 1, dtype="int")
+
+          for i in range(1, len(ySteps)):
+               for j in range(1, len(xSteps)):
+                    # compute the starting and ending (x, y)-coordinates
+                    # for the current block
+                    startX = xSteps[j - 1]
+                    startY = ySteps[i - 1]
+                    endX = xSteps[j]
+                    endY = ySteps[i]
+                    # extract the ROI using NumPy array slicing, compute the
+                    # mean of the ROI, and then draw a rectangle with the
+                    # mean RGB values over the ROI in the original image
+                    roi = image[startY:endY, startX:endX]
+                    (B, G, R) = [int(x) for x in cv2.mean(roi)[:3]]
+                    cv2.rectangle(image, (startX, startY), (endX, endY),(B, G, R), -1)
+
+          return image
