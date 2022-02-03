685a6c6275ff56ac41d9dc7fba7212de42d8e9e1
artkulak <artkulak444@gmail.com>
2022-02-03 15:48:59 +0600

    Setup Script

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ setup(
   author_email="art@artlabs.tech",
   description="A Data Anonymization package for tabular, image and sound data",
   name="anonympy",
-  version="0.2.0",
+  version="0.2.1",
   
   packages=find_packages(exclude=['test']),
-  install_requires=['faker', 'scikit-learn', 'texttable', 'setuptools', 'numpy', 'pandas', 'validators', 'pycryptodome', 'requests', 'pyyaml', 'rfc3339'],
+  install_requires=['faker', 'scikit-learn', 'opencv-python', 'texttable', 'setuptools', 'numpy', 'pandas', 'validators', 'pycryptodome', 'requests', 'pyyaml', 'rfc3339'],
   python_requires='>=3.6*',
   url='https://github.com/ArtLabss/open-data-anonimizer',
   classifiers=[
