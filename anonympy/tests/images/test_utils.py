95659a61ef7c527be424c23880b0857acf6df025
shukkkur <shakhansho.sabzaliev_2023@ucentralasia.org>
2022-03-01 23:02:22 +0600

    Pytests for anonympy/images

---
+++ anonympy/tests/images/test_utils.py
@@ -0,0 +1,59 @@
+import os
+import cv2
+import urllib
+import pytest
+import numpy as np
+from anonympy import __version__
+from anonympy.images.utils import * 
+
+
+@pytest.fixture(scope = 'module')
+def rectangle():
+    x, y, w, h = 2, 7, 8, 1
+    return x, y, w, h
+
+
+@pytest.fixture(scope = 'module')
+def load_image():
+    image = cv2.imread('sad_boy.jpg')
+    return image
+
+
+def test_find_middle(rectangle):
+    x, y, w, h = rectangle
+    
+    output = find_middle(x,y,w,h)
+    expected = (6, 7)
+    
+    assert output == expected, "`find_middle` returned unexpected value for the following arguments: `x=2, y=7, w=8, h=1`"
+    
+
+def test_find_radius(rectangle):
+    x, y, w, h = rectangle
+
+    output = find_radius(x, y, w, h)
+    expected = 4
+
+    assert output == expected, "`find_radius` returned unexpected value for the following arguments: `x=2, y=7, w=8, h=1`"
+
+
+@pytest.mark.skipif(__version__ == '0.2.4', reason="Requires anonympy >= 0.2.5 ")
+def test_sap_noise(load_image):
+    output = sap_noise(load_image, seed = 42)
+    expected = cv2.imread('expected/sap_noise.png')
+    assert np.equal(output, expected), "Image returned by `sap_noise` is different from expected/sap_noise.png"
+    
+
+def test_pixelated(load_image):
+    output = pixelated(os.getcwd())
+
+    expected = cv2.imread('/expected/pixelated.png')
+    assert np.equal(output, expected), "Image returned by `pixelated` is different from expected/pixelated.png"
+
+
+
+
+
+
+
+    
