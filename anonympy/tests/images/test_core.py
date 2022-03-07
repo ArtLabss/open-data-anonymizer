c63b2dd71362eb2ec8057fc4b55122f5216bac37
shukkkur <shakhansho.sabzaliev_2023@ucentralasia.org>
2022-03-07 10:17:42 +0600

    Pytests for `anonympy.images`

--- anonympy/tests/images/test_core.py
+++ anonympy/tests/images/test_core.py
@@ -116,19 +116,38 @@ def test_face_SaP(anonym_img):
     assert is_similar(output, expected), "Image returned by `face_SaP(shape = 'r', box = 'r')` is different from `expected/sap_r_r.png` image"
     
 
+@pytest.mark.skipif(__version__ == '0.2.4', reason="Requires anonympy >= 0.2.5 ")
 def test_face_pixel(anonym_img):
+    output = anonym_img.face_pixel(blocks = 20, box = None)
+    expected = load_image('pixel_none.png')
+    assert is_similar(output, expected), "Image returned by `face_pixel(blocks = 20, box = None)` is different from `expected/pixel_none.png` image"
     
-    
-# from utils import ...
-# face_pixel supports only 'r' 
+    output = anonym_img.face_pixel(blocks = 50, box = None)
+    expected = load_image('pixel_50_none.png')
+    assert is_similar(output, expected), "Image returned by `face_pixel(blocks = 50, box = None)` is different from `expected/pixel_50_none.png` image"
 
+    output = anonym_img.face_pixel(blocks = 20, box = 'r')
+    expected = load_image('pixel_r.png')
+    assert is_similar(output, expected), "Image returned by `face_pixel(blocks = 20, box = 'r')` is different from `expected/pixel_r.png` image"
 
-# 'c' None
-# 'r' None
-# 'c' 'c'
-# 'c' 'r'
-# 'r' 'c'
-# 'r' 'r' 
+    output = anonym_img.face_pixel(blocks = 50, box = 'r')
+    expected = load_image('pixel_50_r.png')
+    assert is_similar(output, expected), "Image returned by `face_pixel(blocks = 50, box = 'r')` is different from `expected/pixel_50_r.png` image"
+            
 
+def test_blur(anonym_img):
+    output = anonym_img.blur()
+    expected = load_image('gaussian_blur.png')
+    assert is_similar(output, expected), "Image returned by `blur(method='gaussian', kernel=(15,15))` is different from `expected/gaussian_blur.png` image"
 
-##assert np.array_equal(output, expected), "Image returned by `sap_noise` is different from expected/sap_noise.png"
+    output = anonym_img.blur('averaging', kernel=(30,30))
+    expected = load_image('averaging_blur.png')
+    assert is_similar(output, expected), "Image returned by `blur('averaging', kernel=(30,30))` is different from `expected/averaging_blur.png` image"
+
+    output = anonym_img.blur('bilateral', kernel=(30, 150, 150))
+    expected = load_image('bilateral_blur.png')
+    assert is_similar(output, expected), "Image returned by `blur('bilateral', kernel=(30, 150, 150))` is different from `expected/bilateral_blur.png` image"
+
+    output = anonym_img.blur('median', kernel=11)
+    expected = load_image('median_blur.png')
+    assert is_similar(output, expected), "Image returned by `blur('median', kernel=11)` is different from `expected/median_blur.png` image"
