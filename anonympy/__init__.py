fd220db3fe4df8cb85623f110d7a0bf0c51df522
artkulak <artkulak444@gmail.com>
2022-04-11 14:59:36 +0600

    Package Description

--- anonympy/__init__.py
+++ anonympy/__init__.py
@@ -3,7 +3,7 @@ Package for data anonymization of different data types
 such as tabular, text, images and sound. 
 '''
 
-__version__ =  "0.2.7"
+__version__ =  "0.3.0"
 
 # Check if all dependencies have been installed
 
