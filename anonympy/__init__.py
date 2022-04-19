ddeb6dcae31b866c529321ca55c0bfa8fe6d72a7
artkulak <artkulak444@gmail.com>
2022-04-19 18:43:56 +0600

    Required Packages

--- anonympy/__init__.py
+++ anonympy/__init__.py
@@ -10,7 +10,7 @@ Package for data anonymization of different data types
 such as tabular, text, images and sound.
 '''
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 # Check if all dependencies have been installed
 hard_dependencies = ("faker", "pandas", "numpy", "cv2", "PyPDF2",
                      "pytesseract", "poppler-utils", "pdf2image",
